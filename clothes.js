8141d4d881d1770eb838c1d1062ecc45225bec60
Your Name In Single Quotes.jumana <your email here in single quotes.mjumana28@yahoo.com>
2019-10-03 14:13:48 +0300

    jakd

--- clothes.js
+++ clothes.js
@@ -1,7 +1,20 @@
-var cloth = prompt("What is your namse?");
-while (cloth != "jojo" ){
+var name = prompt("what your name ?")
+var txt;
+var r = confirm("Are you intrest about fashion?");
+if (r == true) {
+  txt = "You are welcome";
+} else {
+  txt = "it's not your place";
+}
+
+var cloth = prompt("t-shirt or pants!");
+while (cloth != "t-shirt" &&cloth !="pants" ){
   cloth = prompt("sorry enter the correct value :");
 }
-if(cloth === "jojo"){
-alert("correct answer");
+if(cloth === "t-shirt"||cloth === "pants")
+{
+  document.write(cloth)
+
 }
+document.write(name )
+document.write( txt)
